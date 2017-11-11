5fb549b8c18635727ed551839ff7e9ba4e543182
JimmyBot <jmfgdev@users.noreply.github.com>
2017-11-11 08:33:31 -0500

    Update man_deblob.py

--- man_deblob.py
+++ man_deblob.py
@@ -1,45 +1,44 @@
-    #!/usr/bin/env python3
+#!/usr/bin/env python3
 
 """
-Name: man_deblob.py
-Main: JimmyBot(jmfgdev)
-Lisc: GPLV3
+Name: man_deblob.sh
+Main: jadedctrl
+Lisc: ISC
 Desc: Delobbing OBSD man
       pages for use in
       LBSD.
 """
 
-    # Usage: man_deblob.sh $SRC_DIR
+# Usage: man_deblob.sh $SRC_DIR
 
-    . ./libdeblob.sh
+. ./libdeblob.sh
 
-    PATCH_DIR=/tmp/man_deblob
+PATCH_DIR=/tmp/man_deblob
 
 
-    if [ -e $PATCH_DIR ]
-    then
-    	self_destruct_sequence $PATCH_DIR
-    	mkdir $PATCH_DIR
-    else
-    	mkdir $PATCH_DIR
-    fi
+if [ -e $PATCH_DIR ]
+then
+	self_destruct_sequence $PATCH_DIR
+	mkdir $PATCH_DIR
+else
+	mkdir $PATCH_DIR
+fi
 
-    if test -z $1
-    then
-    	SRC_DIR=/usr/src
-    else
-    	SRC_DIR=$1
-    fi
+if test -z $1
+then
+	SRC_DIR=/usr/src
+else
+	SRC_DIR=$1
+fi
 
-    # man4
-    set -A fw_list acx adw adv athn bnx bwi drm fxp inteldrm ips ipw iwi iwm \
-           iwn kue malo myx neo otus pgt ral radeondrm rsu rtwn rum siop tht \
-           thtc ti uath udl ulpt upgt urtwn uvideo wpi yds zyd
-    for man_blob in "${fw_list[@]}"
-    do
-    	strdel "\<${man_blob}.4\>" share/man/man4/Makefile
-    	linedel "\<${man_blob}.4\>" distrib/sets/lists/man/mi
-    done
+# man4
+set -A fw_list acx adw adv athn bnx bwi drm fxp inteldrm ips ipw iwi iwm \
+       iwn kue malo myx neo otus pgt ral radeondrm rsu rtwn rum siop tht \
+       thtc ti uath udl ulpt upgt urtwn uvideo wpi yds zyd
+for man_blob in "${fw_list[@]}"
+do
+	strdel "\<${man_blob}.4\>" share/man/man4/Makefile
+	linedel "\<${man_blob}.4\>" distrib/sets/lists/man/mi
+done
 
-    linedel "./usr/share/man/man1/fw_update.1" distrib/sets/lists/man/mi
-
+linedel "./usr/share/man/man1/fw_update.1" distrib/sets/lists/man/mi
